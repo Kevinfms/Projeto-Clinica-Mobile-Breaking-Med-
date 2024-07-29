c12cc2d17c02e74b18ec83fbd72157bf672a1329
Kevin Moreira <kfmsilva@gmail.com>
2024-07-29 20:47:04 -0300

    att login

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import Login from './src/pages/Login/login';
+import ConsultaMedico from './src/pages/ConsultaMedico/consultaMedico';
+import ConsultaPaciente from './src/pages/ConsultaPaciente/consultaPaciente';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Login' component={Login} options={{title: 'Login', headerShown: false}}/>
+          <Stack.Screen name='ConsultaMedico' component={ConsultaMedico} options={{title: 'Suas Consultas do dia'}}/>
+          <Stack.Screen name='ConsultaPaciente' component={ConsultaPaciente} options={{title: 'Suas Consultas'}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
